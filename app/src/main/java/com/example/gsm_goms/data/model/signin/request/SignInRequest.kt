9ccbe425bead9ff0a570c04e0e6505b713062eda
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-03-24 16:19:42 +0900

    :sparkles: :: data 계층 : gauth 연동 데이터 클래스

---
+++ app/src/main/java/com/example/gsm_goms/data/model/signin/request/SignInRequest.kt
@@ -0,0 +1,5 @@
+package com.example.gsm_goms.data.model.signin.request
+
+data class SignInRequest(
+    val code: String
+)
---
+++ app/src/main/java/com/example/gsm_goms/data/model/signin/response/SignInResponse.kt
@@ -0,0 +1,11 @@
+package com.example.gsm_goms.data.model.signin.response
+
+import com.google.gson.annotations.SerializedName
+import java.time.LocalDateTime
+
+data class SignInResponse(
+    @SerializedName("accessToken") val accessToken: String,
+    @SerializedName("refreshToken") val refreshToken: String,
+    @SerializedName("accessTokenExpiredAt") val accessTokenExpiredAt: LocalDateTime,
+    @SerializedName("refreshTokenExpiredAt") val refreshTokenExpiredAt: LocalDateTime
+)

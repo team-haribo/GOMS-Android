690e82e06e7af09ad1b46e2b7743c0c6e1a2f082
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-04-22 00:04:59 +0900

    :lipstick: :: main page 지각 lazyRow 전용 card 생성

---
+++ presentation/src/main/java/com/goms/presentation/view/component/MainItemCard.kt
@@ -0,0 +1,73 @@
+package com.goms.presentation.view.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.presentation.R
+import com.skydoves.landscapist.coil.CoilImage
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun MainItemCard() {
+    val mainCardFont = FontFamily(
+        Font(R.font.sf_pro_text_regular, FontWeight.Normal),
+        Font(R.font.sf_pro_text_medium, FontWeight.Medium)
+    )
+
+    Card(
+        modifier = Modifier
+            .width(110.dp)
+            .fillMaxHeight()
+            .clip(RoundedCornerShape(10.dp)),
+        onClick = { /*TODO*/ }
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CoilImage(
+                modifier = Modifier.size(40.dp),
+                imageModel = { /*TODO*/ }
+            )
+
+            Text(
+                modifier = Modifier.padding(top = 20.dp),
+                text = "선민재",
+                style = TextStyle(
+                    fontFamily = mainCardFont,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 14.sp
+                )
+            )
+
+            Text(
+                modifier = Modifier.padding(top = 10.dp),
+                text = "3111",
+                style = TextStyle(
+                    fontFamily = mainCardFont,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 12.sp
+                )
+            )
+        }
+    }
+}

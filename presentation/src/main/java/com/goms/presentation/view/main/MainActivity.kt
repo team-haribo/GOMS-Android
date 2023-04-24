786fbef8f91c60fa137fc0db7abf3b9b777ee6cc
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-04-24 23:09:14 +0900

    :sparkles: :: outing student card view 클릭 이벤트 추가

--- presentation/src/main/java/com/goms/presentation/view/main/MainActivity.kt
+++ presentation/src/main/java/com/goms/presentation/view/main/MainActivity.kt
@@ -3,6 +3,8 @@ package com.goms.presentation.view.main
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.presentation.R
@@ -27,12 +29,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
+    private lateinit var navGraph: NavGraph
     private fun setNavigation() {
         val scanAble = intent.getBooleanExtra("scanAble", false)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
-        val navController = navHostFragment.navController
-        val navGraph = navController.navInflater.inflate(R.navigation.bottom_navigation)
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+        navGraph = navController.navInflater.inflate(R.navigation.bottom_navigation)
 
         // start destination 설정하기
         if (scanAble) navGraph.setStartDestination(R.id.outingFragment)
@@ -42,5 +48,19 @@ class MainActivity : AppCompatActivity() {
         navController.setGraph(navGraph, null)
 
         binding.gomsBottomNavigationView.setupWithNavController(navController)
+    }
+
+    fun navigateToOuting() {
+        navController.navigate(R.id.outingFragment)
+
+        binding.gomsBottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homeFragment -> navController.navigate(R.id.homeFragment)
+                R.id.qrScanFragment -> navController.navigate(R.id.qrScanFragment)
+                R.id.outingFragment -> navController.navigate(R.id.outingFragment)
+                else -> return@setOnItemSelectedListener false
+            }
+            return@setOnItemSelectedListener true
+        }
     }
 }

eff7c874a6226fa528a80952454856f4c1346b05
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-04-25 12:01:10 +0900

    :sparkles: :: sign in response에 새로운 data 추가

--- data/src/main/java/com/goms/data/model/signin/response/SignInResponse.kt
+++ data/src/main/java/com/goms/data/model/signin/response/SignInResponse.kt
@@ -6,6 +6,7 @@ import java.time.LocalDateTime
 data class SignInResponse(
     @SerializedName("accessToken") val accessToken: String,
     @SerializedName("refreshToken") val refreshToken: String,
-    @SerializedName("accessTokenExpiredAt") val accessTokenExpiredAt: LocalDateTime,
-    @SerializedName("refreshTokenExpiredAt") val refreshTokenExpiredAt: LocalDateTime
+    @SerializedName("accessTokenExpiredAt") val accessTokenExpiredAt: LocalDateTime?,
+    @SerializedName("refreshTokenExpiredAt") val refreshTokenExpiredAt: LocalDateTime?,
+    @SerializedName("authority") val authority: String
 )
--- domain/src/main/java/com/goms/domain/data/signin/response/SignInResponseData.kt
+++ domain/src/main/java/com/goms/domain/data/signin/response/SignInResponseData.kt
@@ -5,6 +5,7 @@ import java.time.LocalDateTime
 data class SignInResponseData(
     val accessToken: String,
     val refreshToken: String,
-    val accessTokenExpiredAt: LocalDateTime,
-    val refreshTokenExpiredAt: LocalDateTime
+    val accessTokenExpiredAt: LocalDateTime?,
+    val refreshTokenExpiredAt: LocalDateTime?,
+    val authority: String
 )

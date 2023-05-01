fa72e0ff153dddfa57f9e4a99cb2293ecbd19675
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-05-01 17:48:49 +0900

    :sparkles: :: profile api data source 생성

---
+++ data/src/main/java/com/goms/data/datasource/profile/ProfileDataSourceImpl.kt
@@ -0,0 +1,17 @@
+package com.goms.data.datasource.profile
+
+import com.goms.data.api.ProfileApi
+import com.goms.data.model.profile.response.ProfileResponse
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class ProfileDataSourceImpl @Inject constructor(
+    private val profileApi: ProfileApi
+): ProfileDataSource {
+    override suspend fun getProfile(accessToken: String): Flow<ProfileResponse> {
+        return flow {
+            emit(profileApi.getProfile(accessToken))
+        }
+    }
+}

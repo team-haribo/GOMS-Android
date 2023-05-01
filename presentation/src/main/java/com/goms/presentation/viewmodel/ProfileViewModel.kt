ecf29ee66529641572a8061013ecfc8634611089
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-05-01 17:53:25 +0900

    :sparkles: :: 학생 정보 view model 생성

---
+++ presentation/src/main/java/com/goms/presentation/viewmodel/ProfileViewModel.kt
@@ -0,0 +1,38 @@
+package com.goms.presentation.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.goms.domain.data.profile.response.ProfileResponseData
+import com.goms.domain.usecase.profile.ProfileUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileViewModel @Inject constructor(
+    private val profileUseCase: ProfileUseCase
+): ViewModel() {
+    private val _profile: MutableStateFlow<ProfileResponseData?> = MutableStateFlow(null)
+    val profile: StateFlow<ProfileResponseData?> = _profile
+
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> = _isLoading
+
+    fun getProfileLogic() = viewModelScope.launch {
+        profileUseCase().onStart {
+            _isLoading.value = true
+        }.onCompletion {
+            _isLoading.value = false
+        }.catch {
+            Log.e("TAG", "getProfileLogic: ${it.printStackTrace()}", it.cause)
+        }.collect {
+            _profile.value = it
+        }
+    }
+}

5eef60fdfc2a297b21b6cd46037116ac6e38c090
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-05-01 10:00:46 +0900

    :sparkles: :: 토큰 재발급 api service 생성

---
+++ data/src/main/java/com/goms/data/api/AuthService.kt
@@ -0,0 +1,21 @@
+package com.goms.data.api
+
+import com.goms.data.model.auth.request.SignInRequest
+import com.goms.data.model.auth.response.RefreshTokenResponse
+import com.goms.data.model.auth.response.SignInResponse
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.PATCH
+import retrofit2.http.POST
+
+interface AuthService {
+    @POST("auth/signin")
+    suspend fun signIn(
+        @Body body: SignInRequest
+    ): SignInResponse
+
+    @PATCH("auth")
+    suspend fun refreshToken(
+        @Header("refreshToken") refreshToken: String
+    ): RefreshTokenResponse
+}

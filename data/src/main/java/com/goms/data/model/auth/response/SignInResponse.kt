617fe01399ecf5d51aae232bdae2188936a491fa
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-05-01 10:17:01 +0900

    :sparkles: :: package 이동

--- data/src/main/java/com/goms/data/model/auth/response/SignInResponse.kt
+++ data/src/main/java/com/goms/data/model/auth/response/SignInResponse.kt
@@ -1,12 +1,11 @@
-package com.goms.data.model.signin.response
+package com.goms.data.model.auth.response
 
 import com.google.gson.annotations.SerializedName
-import java.time.LocalDateTime
 
 data class SignInResponse(
     @SerializedName("accessToken") val accessToken: String,
     @SerializedName("refreshToken") val refreshToken: String,
-    @SerializedName("accessTokenExpiredAt") val accessTokenExpiredAt: LocalDateTime?,
-    @SerializedName("refreshTokenExpiredAt") val refreshTokenExpiredAt: LocalDateTime?,
+    @SerializedName("accessTokenExp") val accessTokenExpiredAt: String?,
+    @SerializedName("refreshTokenExp") val refreshTokenExpiredAt: String?,
     @SerializedName("authority") val authority: String
 )

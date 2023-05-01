38f4891312241e866dbd5d30964ace917a165809
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-05-01 10:22:48 +0900

    :sparkles: :: 로그인 뷰모델에 토큰 재발급 관련 로직 추가하기

--- presentation/src/main/java/com/goms/presentation/viewmodel/SignInViewModel.kt
+++ presentation/src/main/java/com/goms/presentation/viewmodel/SignInViewModel.kt
@@ -4,7 +4,11 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.goms.domain.data.signin.response.SignInResponseData
-import com.goms.domain.usecase.SignInUseCase
+import com.goms.domain.exception.NotRequestParamException
+import com.goms.domain.exception.OtherException
+import com.goms.domain.exception.ServerException
+import com.goms.domain.usecase.auth.SetTokenUseCase
+import com.goms.domain.usecase.auth.SignInUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -12,11 +16,13 @@ import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
+import retrofit2.HttpException
 import javax.inject.Inject
 
 @HiltViewModel
 class SignInViewModel @Inject constructor(
-    private val signInUseCase: SignInUseCase
+    private val signInUseCase: SignInUseCase,
+    private val setTokenUseCase: SetTokenUseCase
 ): ViewModel() {
     private val _signIn = MutableStateFlow<SignInResponseData?>(null)
     val signIn: StateFlow<SignInResponseData?> = _signIn
@@ -30,8 +36,23 @@ class SignInViewModel @Inject constructor(
         }.onCompletion {
             _isLoading.value = false
         }.catch { error ->
-            Log.e("TAG", "signInLogic: ${error.printStackTrace()}", error.cause)
+            if (error is HttpException) {
+                val message = error.message
+                throw when(error.code()) {
+                    400 -> NotRequestParamException(message = message)
+                    500 -> ServerException(message = message)
+                    else -> OtherException(
+                        code = error.code(),
+                        message = message
+                    )
+                }
+            } else Log.e("TAG", "signInLogic: ${error.printStackTrace()}", error.cause)
         }.collect { response ->
+            setTokenUseCase(
+                accessToken = response.accessToken,
+                refreshToken = response.refreshToken,
+                accessTokenExp = response.accessTokenExp.toString()
+            )
             _signIn.value = response
         }
     }

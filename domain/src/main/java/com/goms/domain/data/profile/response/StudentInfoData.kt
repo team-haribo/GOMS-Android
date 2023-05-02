b0175db4591fd577be128e405fe06979b1fede7b
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-05-02 22:09:31 +0900

    :sparkles: :: profile response serializable 추가

--- domain/src/main/java/com/goms/domain/data/profile/response/StudentInfoData.kt
+++ domain/src/main/java/com/goms/domain/data/profile/response/StudentInfoData.kt
@@ -1,7 +1,9 @@
 package com.goms.domain.data.profile.response
 
+import java.io.Serializable
+
 data class StudentInfoData(
     val grade: Int,
     val classNum: Int,
     val number: Int
-)
+): Serializable

899437a10fcfe8b116db4dc2a054e8605d2ca55a
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-05-03 23:15:47 +0900

    :sparkles: :: late ranking 리스트 구현

--- presentation/src/main/java/com/goms/presentation/view/home/component/HomeItemCard.kt
+++ presentation/src/main/java/com/goms/presentation/view/home/component/HomeItemCard.kt
@@ -7,15 +7,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -23,11 +22,11 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.presentation.R
+import com.goms.domain.data.profile.response.ProfileResponseData
 import com.skydoves.landscapist.coil.CoilImage
 
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun HomeItemCard() {
+fun HomeItemCard(item: ProfileResponseData) {
     val homeCardFont = FontFamily(
         Font(R.font.sf_pro_text_regular, FontWeight.Normal),
         Font(R.font.sf_pro_text_medium, FontWeight.Medium)
@@ -45,13 +44,15 @@ fun HomeItemCard() {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             CoilImage(
-                modifier = Modifier.size(40.dp),
-                imageModel = { R.drawable.user_profile }
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape),
+                imageModel = { item.profileUrl ?: R.drawable.user_profile }
             )
 
             Text(
                 modifier = Modifier.padding(top = 15.dp),
-                text = "선민재",
+                text = item.name,
                 style = TextStyle(
                     fontFamily = homeCardFont,
                     fontWeight = FontWeight.Medium,
@@ -59,9 +60,10 @@ fun HomeItemCard() {
                 )
             )
 
+            val studentInfo = item.studentNum
             Text(
                 modifier = Modifier.padding(top = 7.dp),
-                text = "3111",
+                text = "${studentInfo.grade}${studentInfo.classNum}${studentInfo.number}",
                 style = TextStyle(
                     fontFamily = homeCardFont,
                     fontWeight = FontWeight.Normal,

ca433905e8b09f85cf0a0e6a2804f39a8ffad0a2
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-05-05 23:30:33 +0900

    :sparkles: :: 처음 앱을 실행했을 때 외출 관리 sharedPreference 초기화 로직 생성

--- presentation/src/main/java/com/goms/presentation/view/splash/SplashActivity.kt
+++ presentation/src/main/java/com/goms/presentation/view/splash/SplashActivity.kt
@@ -2,6 +2,7 @@ package com.goms.presentation.view.splash
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.SharedPreferences
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Bundle
@@ -20,10 +21,15 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
     private val splashViewModel by viewModels<SplashViewModel>()
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        sharedPreferences = getSharedPreferences("userOuting", MODE_PRIVATE)
+        if (sharedPreferences.contains("userOuting"))
+            setSharedPreference()
 
         Handler(Looper.getMainLooper()).postDelayed({
             if (checkIsInterConnected()) {
@@ -31,6 +37,12 @@ class SplashActivity : AppCompatActivity() {
                 observeLogin()
             } else Toast.makeText(this, "인터넷 없음", Toast.LENGTH_SHORT).show()
         }, 1000)
+    }
+
+    private fun setSharedPreference() {
+        sharedPreferences.edit()
+            .putBoolean("outingStatus", false)
+            .apply()
     }
 
     private fun observeLogin() {

f1d5a7a2bd99c93cdcd38bd53e2575edb8019eba
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-05-07 17:22:21 +0900

    :sparkles: :: 외출하기 data source uuid path 추가

--- data/src/main/java/com/goms/data/datasource/outing/OutingDataSource.kt
+++ data/src/main/java/com/goms/data/datasource/outing/OutingDataSource.kt
@@ -3,9 +3,10 @@ package com.goms.data.datasource.outing
 import com.goms.data.model.outing.OutingCountResponse
 import com.goms.data.model.profile.ProfileResponse
 import kotlinx.coroutines.flow.Flow
+import java.util.UUID
 
 interface OutingDataSource {
-    suspend fun outing()
+    suspend fun outing(outingUUID: UUID)
 
     suspend fun getOutingList(): Flow<List<ProfileResponse>>
 
--- data/src/main/java/com/goms/data/datasource/outing/OutingDataSourceImpl.kt
+++ data/src/main/java/com/goms/data/datasource/outing/OutingDataSourceImpl.kt
@@ -5,13 +5,14 @@ import com.goms.data.model.outing.OutingCountResponse
 import com.goms.data.model.profile.ProfileResponse
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import java.util.UUID
 import javax.inject.Inject
 
 class OutingDataSourceImpl @Inject constructor(
     private val outingApi: OutingApi
 ): OutingDataSource {
-    override suspend fun outing() {
-        return outingApi.outing()
+    override suspend fun outing(outingUUID: UUID) {
+        return outingApi.outing(outingUUID)
     }
 
     override suspend fun getOutingList(): Flow<List<ProfileResponse>> {

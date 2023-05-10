61dd527576ff1b793c7ba870f25ca5ec755b7cbb
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-05-11 08:24:53 +0900

    :recycle: :: admin 여부 판단 외장 함수 이름 변경

--- presentation/src/main/java/com/goms/presentation/utils/CheckRole.kt
+++ presentation/src/main/java/com/goms/presentation/utils/CheckRole.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.content.Context.MODE_PRIVATE
 
 // 역할이 admin인지 확인합니다.
-fun getUserIsAdmin(context: Context): Boolean {
+fun checkUserIsAdmin(context: Context): Boolean {
     val authorityPreferences = context.getSharedPreferences("authority", MODE_PRIVATE)
     val role = authorityPreferences.getString("role", "").toString()
 

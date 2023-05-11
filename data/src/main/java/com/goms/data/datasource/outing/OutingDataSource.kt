05a5a39a05f61487472f42bf5d9a451c7d1009dd
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-05-11 14:07:16 +0900

    :recycle: :: outing list data source return type 변경

--- data/src/main/java/com/goms/data/datasource/outing/OutingDataSource.kt
+++ data/src/main/java/com/goms/data/datasource/outing/OutingDataSource.kt
@@ -1,14 +1,14 @@
 package com.goms.data.datasource.outing
 
 import com.goms.data.model.outing.OutingCountResponse
-import com.goms.data.model.profile.ProfileResponse
+import com.goms.data.model.user.UserResponse
 import kotlinx.coroutines.flow.Flow
 import java.util.UUID
 
 interface OutingDataSource {
     suspend fun outing(outingUUID: UUID)
 
-    suspend fun getOutingList(): Flow<List<ProfileResponse>>
+    suspend fun getOutingList(): Flow<List<UserResponse>>
 
     suspend fun getOutingCount(): Flow<OutingCountResponse>
 }

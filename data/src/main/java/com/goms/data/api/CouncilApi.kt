0adb7799cc8ae4a0c51bc2c211396ef7de4713e2
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-05-12 14:47:31 +0900

    :sparkles: :: 권한 변경 api service 추가

--- data/src/main/java/com/goms/data/api/CouncilApi.kt
+++ data/src/main/java/com/goms/data/api/CouncilApi.kt
@@ -1,9 +1,18 @@
 package com.goms.data.api
 
+import com.goms.data.model.council.ModifyRoleRequest
 import com.goms.data.model.user.UserResponse
+import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 
 interface CouncilApi {
     @GET("student-council/account")
     suspend fun getUserList(): List<UserResponse>
+
+    @PATCH("student-council/authority")
+    suspend fun modifyRole(
+        @Body body: ModifyRoleRequest
+    ): Response<Unit>
 }

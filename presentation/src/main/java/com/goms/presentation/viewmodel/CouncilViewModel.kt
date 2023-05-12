7b484fb8f56f224d43aef82761e5d632d5112941
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-05-12 21:09:01 +0900

    :sparkles: :: 외출금지 설정 view model에 로직 추가

--- presentation/src/main/java/com/goms/presentation/viewmodel/CouncilViewModel.kt
+++ presentation/src/main/java/com/goms/presentation/viewmodel/CouncilViewModel.kt
@@ -9,6 +9,7 @@ import com.goms.domain.exception.OtherException
 import com.goms.domain.exception.ServerException
 import com.goms.domain.exception.UserNotFoundException
 import com.goms.domain.usecase.admin.ModifyRoleUseCase
+import com.goms.domain.usecase.admin.SetBlackListUseCase
 import com.goms.domain.usecase.admin.UserListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -16,18 +17,23 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
+import java.util.UUID
 import javax.inject.Inject
 
 @HiltViewModel
 class CouncilViewModel @Inject constructor(
     private val userListUseCase: UserListUseCase,
-    private val modifyRoleUseCase: ModifyRoleUseCase
+    private val modifyRoleUseCase: ModifyRoleUseCase,
+    private val setBlackListUseCase: SetBlackListUseCase
 ): ViewModel() {
     private val _userList: MutableStateFlow<List<UserResponseData>?> = MutableStateFlow(null)
     val userList: StateFlow<List<UserResponseData>?> = _userList
 
     private val _modifyRole: MutableStateFlow<Boolean> = MutableStateFlow(false)
     val modifyRole: StateFlow<Boolean> = _modifyRole
+
+    private val _setBlackList: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val setBlackList: StateFlow<Boolean> = _setBlackList
 
     fun getUserList() {
         viewModelScope.launch {
@@ -56,6 +62,22 @@ class CouncilViewModel @Inject constructor(
                 } else throw OtherException(it.message)
             }.collect {
                 _modifyRole.value = true
+            }
+        }
+    }
+
+    fun setBlackList(accountIdx: UUID) {
+        viewModelScope.launch {
+            setBlackListUseCase(accountIdx).catch {
+                if (it is HttpException) {
+                    when (it.code()) {
+                        403 -> throw NotCouncilException("학생회 계정이 아닙니다.")
+                        404 -> throw UserNotFoundException("계정을 찾을 수 없습니다.")
+                        500 -> throw ServerException("서버 에러")
+                    }
+                } else throw OtherException(it.message)
+            }.collect {
+                _setBlackList.value = true
             }
         }
     }

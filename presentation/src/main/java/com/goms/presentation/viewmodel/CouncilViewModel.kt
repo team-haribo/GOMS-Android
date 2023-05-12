4dcd6d1e4d5ba862f3f590111f0dc4fc34cd90f9
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-05-12 14:57:37 +0900

    :sparkles: :: 권한 수정 로직 생성

--- presentation/src/main/java/com/goms/presentation/viewmodel/CouncilViewModel.kt
+++ presentation/src/main/java/com/goms/presentation/viewmodel/CouncilViewModel.kt
@@ -3,8 +3,12 @@ package com.goms.presentation.viewmodel
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.goms.domain.data.council.ModifyRoleRequestData
 import com.goms.domain.data.user.UserResponseData
+import com.goms.domain.exception.NotCouncilException
 import com.goms.domain.exception.ServerException
+import com.goms.domain.exception.UserNotFoundException
+import com.goms.domain.usecase.admin.ModifyRoleUseCase
 import com.goms.domain.usecase.admin.UserListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -16,22 +20,42 @@ import javax.inject.Inject
 
 @HiltViewModel
 class CouncilViewModel @Inject constructor(
-    private val userListUseCase: UserListUseCase
+    private val userListUseCase: UserListUseCase,
+    private val modifyRoleUseCase: ModifyRoleUseCase
 ): ViewModel() {
     private val _userList: MutableStateFlow<List<UserResponseData>?> = MutableStateFlow(null)
     val userList: StateFlow<List<UserResponseData>?> = _userList
+
+    private val _modifyRole: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val modifyRole: StateFlow<Boolean> = _modifyRole
 
     fun getUserList() {
         viewModelScope.launch {
             userListUseCase().catch {
                 if (it is HttpException) {
                     when (it.code()) {
-                        403 -> Log.d("TAG", "getUserList: 학생회 계정이 아님")
+                        403 -> throw NotCouncilException("학생회 계정이 아닙니다.")
                         500 -> throw ServerException("서버 에러")
                     }
                 } else Log.d("TAG", "getUserList error: $it")
             }.collect { list ->
                 _userList.value = list
+            }
+        }
+    }
+
+    fun modifyRole(body: ModifyRoleRequestData) {
+        viewModelScope.launch {
+            modifyRoleUseCase(body).catch {
+                if (it is HttpException) {
+                    when (it.code()) {
+                        403 -> throw NotCouncilException("학생회 계정이 아닙니다.")
+                        404 -> throw UserNotFoundException("계정을 찾을 수 없습니다.")
+                        500 -> throw ServerException("서버 에러")
+                    }
+                } else Log.d("TAG", "modifyRole error: $it")
+            }.collect {
+                _modifyRole.value = true
             }
         }
     }

fd484e2f777b46d1beb72fbb2fa0c3a215bcfc9d
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-05-15 10:01:28 +0900

    :sparkles: :: search student use case 생성

---
+++ domain/src/main/java/com/goms/domain/usecase/admin/SearchStudentUseCase.kt
@@ -0,0 +1,21 @@
+package com.goms.domain.usecase.admin
+
+import com.goms.domain.data.council.response.SearchStudentResponseData
+import com.goms.domain.repository.CouncilRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class SearchStudentUseCase @Inject constructor(
+    private val councilRepository: CouncilRepository
+) {
+    suspend operator fun invoke(
+        grade: Int,
+        classNum: Int,
+        number: Int,
+        name: String,
+        isBlackList: Boolean,
+        authority: String
+    ): Flow<List<SearchStudentResponseData>> {
+        return councilRepository.searchStudent(grade, classNum, number, name, isBlackList, authority)
+    }
+}

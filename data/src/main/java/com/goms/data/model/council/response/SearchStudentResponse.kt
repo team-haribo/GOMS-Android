287ea4cd8461837eff986aa14e1e83644eb686d2
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-05-15 09:31:27 +0900

    :sparkles: :: 학생 검색 response data 생성

---
+++ data/src/main/java/com/goms/data/model/council/response/SearchStudentResponse.kt
@@ -0,0 +1,13 @@
+package com.goms.data.model.council.response
+
+import com.goms.data.model.user.UserInfo
+import com.google.gson.annotations.SerializedName
+import java.util.UUID
+
+data class SearchStudentResponse(
+    @SerializedName("accountIdx") val accountIdx: UUID,
+    @SerializedName("name") val name: String,
+    @SerializedName("studentNum") val studentNum: UserInfo,
+    @SerializedName("profileUrl") val profileUrl: String,
+    @SerializedName("authority") val authority: String
+)

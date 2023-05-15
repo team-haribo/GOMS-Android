375e5e6765f2fd742f040f79a5c677d868791e0a
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-05-15 17:59:55 +0900

    :sparkles: :: qr code screen qr code 생성 로직 구현

--- presentation/src/main/java/com/goms/presentation/view/qr_scan/QrScanFragment.kt
+++ presentation/src/main/java/com/goms/presentation/view/qr_scan/QrScanFragment.kt
@@ -6,10 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.example.presentation.BuildConfig
 import com.example.presentation.databinding.FragmentQrScanBinding
+import com.goms.presentation.utils.checkUserIsAdmin
 import com.goms.presentation.view.qr_scan.capture.QrCodeActivity
+import com.goms.presentation.viewmodel.CouncilViewModel
+import com.google.zxing.BarcodeFormat
+import com.journeyapps.barcodescanner.BarcodeEncoder
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import java.util.UUID
 
+@AndroidEntryPoint
 class QrScanFragment : Fragment() {
+    private val councilViewModel by viewModels<CouncilViewModel>()
     private lateinit var binding: FragmentQrScanBinding
 
     override fun onCreateView(
@@ -18,8 +30,22 @@ class QrScanFragment : Fragment() {
         savedInstanceState: Bundle?)
     : View {
         binding = FragmentQrScanBinding.inflate(layoutInflater)
-        context?.startActivity(Intent(context, QrCodeActivity::class.java))
+
+        lifecycleScope.launch {
+            if (checkUserIsAdmin(requireContext())) {
+                councilViewModel.makeQrCode()
+                councilViewModel.makeQr.collect { uuid ->
+                    if (uuid != null) createQrCode(uuid.outingUUID)
+                }
+            } else context?.startActivity(Intent(context, QrCodeActivity::class.java))
+        }
 
         return binding.root
+    }
+
+    private fun createQrCode(uuid: UUID?) {
+        val barCodeEncoder = BarcodeEncoder()
+        val bitmap = barCodeEncoder.encodeBitmap(BuildConfig.BASE_URL+uuid, BarcodeFormat.QR_CODE, 300, 300)
+        binding.outingQrCodeImage.setImageBitmap(bitmap)
     }
 }

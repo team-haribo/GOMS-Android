c44e6fc2f77fdb6bd750773ec46c22f2fee58513
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-05-16 17:43:44 +0900

    :recycle: :: 변경된 user list response use case에 적용

--- domain/src/main/java/com/goms/domain/usecase/admin/UserListUseCase.kt
+++ domain/src/main/java/com/goms/domain/usecase/admin/UserListUseCase.kt
@@ -1,6 +1,6 @@
 package com.goms.domain.usecase.admin
 
-import com.goms.domain.data.user.UserResponseData
+import com.goms.domain.data.council.response.UserListResponseData
 import com.goms.domain.repository.CouncilRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -8,7 +8,7 @@ import javax.inject.Inject
 class UserListUseCase @Inject constructor(
     private val councilRepository: CouncilRepository
 ) {
-    suspend operator fun invoke(): Flow<List<UserResponseData>> {
+    suspend operator fun invoke(): Flow<List<UserListResponseData>> {
         return councilRepository.getUserList()
     }
 }

6445f936f1de276804994782b0e6e69c8c7d689e
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-05-16 16:54:35 +0900

    :recycle: :: user 학번 정보 mapper 기존 mapper에 적용

--- data/src/main/java/com/goms/data/mapper/ProfileMapper.kt
+++ data/src/main/java/com/goms/data/mapper/ProfileMapper.kt
@@ -1,26 +1,17 @@
 package com.goms.data.mapper
 
+import com.goms.data.mapper.util.StudentInfoMapper
 import com.goms.data.model.profile.ProfileResponse
-import com.goms.data.model.profile.StudentInfo
 import com.goms.domain.data.profile.ProfileResponseData
-import com.goms.domain.data.profile.StudentInfoData
 
 object ProfileMapper {
     fun profileResponseToData(profileResponse: ProfileResponse): ProfileResponseData {
         return ProfileResponseData(
             accountIdx = profileResponse.accountIdx,
             name = profileResponse.name,
-            studentNum = studentInfoToData(profileResponse.studentNum),
+            studentNum = StudentInfoMapper.studentInfoToData(profileResponse.studentNum),
             profileUrl = profileResponse.profileUrl,
             lateCount = profileResponse.lateCount
-        )
-    }
-
-    private fun studentInfoToData(studentInfo: StudentInfo): StudentInfoData {
-        return StudentInfoData(
-            grade = studentInfo.grade,
-            classNum = studentInfo.classNum,
-            number = studentInfo.number
         )
     }
 }

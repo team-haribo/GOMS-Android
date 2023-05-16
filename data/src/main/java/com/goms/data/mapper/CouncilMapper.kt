292f11a5f7fd95866c9509710bf5353f58568d26
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-05-16 17:41:58 +0900

    :sparkles: :: 변경된 user list response mapper 생성

--- data/src/main/java/com/goms/data/mapper/CouncilMapper.kt
+++ data/src/main/java/com/goms/data/mapper/CouncilMapper.kt
@@ -3,10 +3,23 @@ package com.goms.data.mapper
 import com.goms.data.mapper.util.StudentInfoMapper
 import com.goms.data.model.council.request.ModifyRoleRequest
 import com.goms.data.model.council.response.SearchStudentResponse
+import com.goms.data.model.council.response.UserListResponse
 import com.goms.domain.data.council.request.ModifyRoleRequestData
 import com.goms.domain.data.council.response.SearchStudentResponseData
+import com.goms.domain.data.council.response.UserListResponseData
 
 object CouncilMapper {
+    fun userListResponseToData(userListResponse: UserListResponse): UserListResponseData {
+        return UserListResponseData(
+            accountIdx = userListResponse.accountIdx,
+            name = userListResponse.name,
+            studentNum = StudentInfoMapper.studentInfoToData(userListResponse.studentNum),
+            profileUrl = userListResponse.profileUrl,
+            authority = userListResponse.authority,
+            isBlackList = userListResponse.isBlackList
+        )
+    }
+
     fun modifyRoleRequestToDomain(modifyRoleRequestData: ModifyRoleRequestData): ModifyRoleRequest {
         return ModifyRoleRequest(
             accountIdx = modifyRoleRequestData.accountIdx,

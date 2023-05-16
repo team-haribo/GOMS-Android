e508fa1eb8c8c62262dcab383b31d8f7fbd1e302
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-05-16 17:41:41 +0900

    :sparkles: :: 변경된 user list response data 생성

---
+++ data/src/main/java/com/goms/data/model/council/response/UserListResponse.kt
@@ -0,0 +1,14 @@
+package com.goms.data.model.council.response
+
+import com.goms.data.model.util.StudentInfo
+import com.google.gson.annotations.SerializedName
+import java.util.UUID
+
+data class UserListResponse(
+    @SerializedName("accountIdx") val accountIdx: UUID,
+    @SerializedName("name") val name: String,
+    @SerializedName("studentNum") val studentNum: StudentInfo,
+    @SerializedName("profileUrl") val profileUrl: String?,
+    @SerializedName("authority") val authority: String,
+    @SerializedName("isBlackList") val isBlackList: Boolean
+)

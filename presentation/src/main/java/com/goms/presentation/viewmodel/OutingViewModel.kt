6afd690ecc25c319c7b3f067d1311c1dc7e04df7
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-05-30 20:37:20 +0900

    :recycle: :: view model에 외출하기/복귀하기 예외 처리 로직 변경

--- presentation/src/main/java/com/goms/presentation/viewmodel/OutingViewModel.kt
+++ presentation/src/main/java/com/goms/presentation/viewmodel/OutingViewModel.kt
@@ -1,20 +1,17 @@
 package com.goms.presentation.viewmodel
 
-import android.content.Context
-import android.content.Context.MODE_PRIVATE
 import androidx.lifecycle.ViewModel
 import com.goms.domain.data.outing.OutingCountResponseData
 import com.goms.domain.data.user.UserResponseData
 import com.goms.domain.exception.FailAccessTokenException
-import com.goms.domain.exception.QrCodeExpiredException
 import com.goms.domain.exception.OtherException
+import com.goms.domain.exception.QrCodeExpiredException
 import com.goms.domain.exception.ServerException
 import com.goms.domain.exception.UserIsBlackListException
 import com.goms.domain.usecase.outing.OutingCountUseCase
 import com.goms.domain.usecase.outing.OutingListUseCase
 import com.goms.domain.usecase.outing.OutingUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
@@ -27,7 +24,6 @@ import javax.inject.Inject
 
 @HiltViewModel
 class OutingViewModel @Inject constructor(
-    @ApplicationContext context: Context,
     private val outingUseCase: OutingUseCase,
     private val outingListUseCase: OutingListUseCase,
     private val outingCountUseCase: OutingCountUseCase
@@ -45,14 +41,15 @@ class OutingViewModel @Inject constructor(
     val isLoading: StateFlow<Boolean> = _isLoading
 
     suspend fun outingLogic(outingUUID: UUID) {
-        outingUseCase(outingUUID).catch {
-            _isOuting.value = false
-            if (it is HttpException) {
+        outingUseCase(outingUUID).collect {
+            if (it.isSuccessful)
+                _isOuting.value = true
+            else {
+                val errorBody = it.errorBody()?.string()
+                val jsonObject = JSONObject(errorBody!!)
+
                 when(it.code()) {
                     400 -> {
-                        val errorBody = it.response()?.errorBody()?.string()
-                        val jsonObject = JSONObject(errorBody!!)
-
                         when(jsonObject.getString("message")) {
                             "검증되지 않은 외출 식별자 입니다." -> throw QrCodeExpiredException("올바르지 않은 Qr Code입니다.")
                             "블랙리스트인 학생은 외출을 할 수 없습니다." -> throw UserIsBlackListException("외출 금지인 사용자입니다.")
@@ -61,11 +58,7 @@ class OutingViewModel @Inject constructor(
                     401 -> throw FailAccessTokenException("access token이 유효하지 않습니다")
                     500 -> throw ServerException("서버 에러")
                 }
-            } else if (it is IllegalArgumentException) throw QrCodeExpiredException("올바르지 않은 QR Code입니다.")
-            else throw OtherException(it.message)
-        }.collect {
-            manageOutingSharedPreference()
-            _isOuting.value = true
+            }
         }
     }
 
@@ -100,16 +93,5 @@ class OutingViewModel @Inject constructor(
         }.collect {
             _outingCount.value = it
         }
-    }
-
-    // 현재 외출 중인지 상태를 체크한다.
-    private val sharedPreferences = context.getSharedPreferences("userOuting", MODE_PRIVATE)
-    private val editor = sharedPreferences.edit()
-
-    private fun manageOutingSharedPreference() {
-        if (!sharedPreferences.getBoolean("outingStatus", false))
-            editor.putBoolean("outingStatus", true)
-        else editor.putBoolean("outingStatus", false)
-        editor.apply()
     }
 }

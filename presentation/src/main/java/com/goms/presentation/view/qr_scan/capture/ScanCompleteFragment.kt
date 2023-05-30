8eceafe9d52cc1c67e4ce30ab278bf93036ca236
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-05-30 10:41:14 +0900

    :lipstick: :: 외출 text center 정렬하기

--- presentation/src/main/java/com/goms/presentation/view/qr_scan/capture/ScanCompleteFragment.kt
+++ presentation/src/main/java/com/goms/presentation/view/qr_scan/capture/ScanCompleteFragment.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.sp
 import com.example.presentation.R
@@ -73,7 +74,8 @@ class ScanCompleteFragment : Fragment() {
                         else "무사히 복귀하였습니다."
                     )
                 }
-            }
+            },
+            textAlign = TextAlign.Center
         )
     }
 }

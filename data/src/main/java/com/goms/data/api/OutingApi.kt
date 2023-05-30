a9d7efd586a4f0a01f050b25267cdd97edc8f4d9
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-05-30 20:35:04 +0900

    :recycle: :: 외출하기/복귀하기 response type 변경

--- data/src/main/java/com/goms/data/api/OutingApi.kt
+++ data/src/main/java/com/goms/data/api/OutingApi.kt
@@ -2,6 +2,7 @@ package com.goms.data.api
 
 import com.goms.data.model.outing.OutingCountResponse
 import com.goms.data.model.user.UserResponse
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
@@ -9,7 +10,7 @@ import java.util.UUID
 
 interface OutingApi {
     @POST("outing/{outingUUID}")
-    suspend fun outing(@Path("outingUUID") outingUUID: UUID)
+    suspend fun outing(@Path("outingUUID") outingUUID: UUID): Response<Unit>
 
     @GET("outing")
     suspend fun getOutingList(): List<UserResponse>

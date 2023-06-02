67bfeec73ac21ee553d56aa47b72f46435448548
박성현 <pjhomg5530@naver.com>
2023-06-02 15:54:47 +0900

    :green_heart: :: versionCode 올리기

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.goms.presentation"
         minSdk = 30
         targetSdk = 33
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"

ab0c2d0a4a570a9a4f1de5af45f4986948568915
박성현 <pjhomg5530@naver.com>
2023-06-02 14:20:51 +0900

    :green_heart: :: versionName 올리기

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 30
         targetSdk = 33
         versionCode = 1
-        versionName = "1.0.1"
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 

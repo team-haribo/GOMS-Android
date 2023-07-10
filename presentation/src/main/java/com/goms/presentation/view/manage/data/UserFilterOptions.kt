a1d023712e6bff649a8c4362a4db680ac21f616a
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-07-10 12:23:34 +0900

    :sparkles: :: 사용자 필터 정보를 저장할 data class 생성

---
+++ presentation/src/main/java/com/goms/presentation/view/manage/data/UserFilterOptions.kt
@@ -0,0 +1,9 @@
+package com.goms.presentation.view.manage.data
+
+data class UserFilterOptions(
+    val grade: Int?,
+    val classNum: Int?,
+    val name: String?,
+    val isBlackList: Boolean?,
+    val authority: String?
+)

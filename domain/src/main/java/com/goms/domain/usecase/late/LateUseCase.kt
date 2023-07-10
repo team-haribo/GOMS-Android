c6e859037d41a865f33f0fee627636bda66ca21d
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-07-10 18:13:13 +0900

    :recycle: :: 기존의 late ranking api return type 변경

--- domain/src/main/java/com/goms/domain/usecase/late/LateUseCase.kt
+++ domain/src/main/java/com/goms/domain/usecase/late/LateUseCase.kt
@@ -1,6 +1,6 @@
 package com.goms.domain.usecase.late
 
-import com.goms.domain.data.profile.ProfileResponseData
+import com.goms.domain.data.late.LateUserResponseData
 import com.goms.domain.repository.LateRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -8,7 +8,7 @@ import javax.inject.Inject
 class LateUseCase @Inject constructor(
     private val lateRepository: LateRepository
 ) {
-    suspend operator fun invoke(): Flow<List<ProfileResponseData>> {
+    suspend operator fun invoke(): Flow<List<LateUserResponseData>> {
         return lateRepository.getLateRank()
     }
 }

19b7f52717f8415105162734250f96f13ab0c10f
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-08-24 20:54:00 +0900

    :rocket: :: 1.1.1 출시

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.goms.presentation"
         minSdk = 26
         targetSdk = 33
-        versionCode = 6
-        versionName = "1.1.0"
+        versionCode = 7
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 

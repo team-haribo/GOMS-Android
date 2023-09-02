bb3d1165f1fc428ebf119741cddd7dd7a34649dc
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-09-02 23:45:04 +0900

    :fire: :: accountIdx 제거

--- data/src/main/java/com/goms/data/model/profile/ProfileResponse.kt
+++ data/src/main/java/com/goms/data/model/profile/ProfileResponse.kt
@@ -2,10 +2,8 @@ package com.goms.data.model.profile
 
 import com.goms.data.model.util.StudentInfo
 import com.google.gson.annotations.SerializedName
-import java.util.UUID
 
 data class ProfileResponse(
-    @SerializedName("accountIdx") val accountIdx: UUID,
     @SerializedName("name") val name: String,
     @SerializedName("studentNum") val studentNum: StudentInfo,
     @SerializedName("authority") val authority: String,
